4bbce63637aab9e4cb4be360dd2c3c36a3ccb882
mohitgaur <mohitgaur@192.168.0.106>
2022-10-07 17:20:46 +0530

    save

--- src/test/java/Mobile_Browser_TC_01.java
+++ src/test/java/Mobile_Browser_TC_01.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
 public class Mobile_Browser_TC_01 extends Mobile_Browser_Base_File
@@ -5,7 +7,9 @@ public class Mobile_Browser_TC_01 extends Mobile_Browser_Base_File
 @Test
 	public  void Browser_TC01()
 	{
-		
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Mohit");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);  //click on google search button
 
 	}
 
